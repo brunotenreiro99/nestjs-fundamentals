e086ba1a488852bd0632cdc14b5d93ed332a24c7
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-26 10:26:03 +0000

    feat: adding indexes to entities

--- src/events/entities/event.entity/event.entity.ts
+++ src/events/entities/event.entity/event.entity.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * Indexes allow faster search
+ * We need to use the index decorator in the column
+ * For more advanced search and combinations, we can use at the class level the index decorator with an array
+ */
+
+@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
@@ -8,6 +15,7 @@ export class Event {
   @Column()
   type: string;
 
+  @Index()
   @Column()
   name: string;
 
