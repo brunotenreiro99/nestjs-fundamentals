824924fb68d1d8d2bb8bce00bd4e4b89bd259e5c
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-26 12:45:40 +0000

    feat: schema validation

--- application-configuration.md
+++ application-configuration.md
@@ -5,3 +5,4 @@
     By default ConfigModule.forRoot() looks for ".env" file
         We can set to envFilePath a string or an array of files. If a variable is found in multiple files, the first one found is the one used.
         When deploying to production we may not need a .env file for that case we could use ignoreEnvFile: true. And now it will be ignore intirely.
+        NestJS does not check for changes in the .env, we need to restart or update another ts file
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.enviornment',
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_USER: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_NAME: Joi.required(),
+        DATABASE_POST: Joi.number().default(5432),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
