ea9939dcf8fad3923c338347a7af43b1d3e5ed3d
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-26 12:11:13 +0000

    feat: control providers scope ⭐️

--- src/coffee-rating/coffee-rating.service.ts
+++ src/coffee-rating/coffee-rating.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CoffeesService } from 'src/coffees/coffees.service';
 
-@Injectable()
+@Injectable() // the same as @Injectable({ scope: Scope.DEFAULT })
+/**
+    Control Providers Scope
+
+    By default Injectable sets the scope as singleton, this is the same as writing @Injectable({ scope: Scope.DEFAULT  })
+    When the app boostraps all the singleton providers instantiate
+
+    Transient instances are not shared between consumers, each one recives a dedicated instance:
+        Example: scope: Scope.TRANSIENT enables that feature
+
+    The same can be done in the module itself:
+
+    @Module({
+        imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
+        controllers: [CoffeesController],
+        providers: [
+            CoffeesService,
+            {
+            provide: COFFEE_BRANDS,
+            useFactory: () => ['buddy brew', 'nescafe'],
+            scope: Scope.TRANSIENT,
+            },
+        ],
+        exports: [CoffeesService],
+    })
+
+    Then we have the request scope, this provides an instance of the providers for each request
+    that means it get deleted and instanceted each time its called
+    Example of usage: scope: Scope.REQUEST
+
+ */
 export class CoffeeRatingService {
   constructor(private readonly coffeesService: CoffeesService) {}
 }
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Injectable, Module, Scope } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -27,12 +27,8 @@ export class CoffeeBrandsFactory {
     CoffeesService,
     {
       provide: COFFEE_BRANDS,
-      useFactory: async (connection: Connection): Promise<string[]> => {
-        // const coffeeBrands = await connection.query(...)
-        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
-        return coffeeBrands;
-      },
-      inject: [Connection],
+      useFactory: () => ['buddy brew', 'nescafe'],
+      scope: Scope.TRANSIENT,
     },
   ],
   exports: [CoffeesService],
