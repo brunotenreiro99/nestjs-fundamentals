e603942a9016db59eac3c1969a004e21c51e05ac
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-26 11:57:41 +0000

    feat: control providers scope

---
+++ src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+@Module({})
+
+// Improved Dynamic Module way of creating CONNECTION provider
+export class DatabaseModule {
+  static register(options: DataSourceOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: 'CONNECTION',
+          useValue: new DataSource(options),
+        },
+      ],
+    };
+  }
+}
