d60392e55ec3a16c5a689a847ad42cf5321da125
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-26 11:30:58 +0000

    feat: non-class based provider tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,13 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/flavor.entity.ts/coffee.entity';
 import { Flavor } from './entities/flavor.entity.ts/flavor.entity.ts';
 import { Event } from 'src/events/entities/event.entity/event.entity';
-
-class MockCoffeesService {}
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['brand1', 'brand2'] },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
