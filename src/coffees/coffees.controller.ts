969818df4c98833e2d6279d987571124ceab191a
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-27 10:23:59 +0000

    feat: adding pagination

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeDto } from './dto/create-coffe.dto/create-coffe.dto';
+import { UpdateCoffeDto } from './dto/create-coffe.dto/update-coffe.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQueryDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeDto: CreateCoffeDto) {
+    return this.coffeesService.create(createCoffeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeDto: UpdateCoffeDto) {
+    return this.coffeesService.update(id, updateCoffeDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Coffee } from './entities/flavor.entity.ts/coffee.entity';
+import { Model } from 'mongoose';
+import { CreateCoffeDto } from './dto/create-coffe.dto/create-coffe.dto';
+import { UpdateCoffeDto } from './dto/create-coffe.dto/update-coffe.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
-  constructor() {}
+  constructor(
+    @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+  ) {}
+
+  findAll(paginationQueryDto: PaginationQueryDto): Promise<Coffee[]> {
+    const { offset, limit } = paginationQueryDto;
+    return this.coffeeModel.find().skip(offset).limit(limit).exec();
+  }
+
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
+
+    if (!coffee) {
+      throw new NotFoundException();
+    }
+
+    return coffee;
+  }
+
+  create(createCoffeDto: CreateCoffeDto) {
+    const coffee = new this.coffeeModel(createCoffeDto);
+    return coffee.save();
+  }
+
+  async update(id: string, updateCoffeeDto: UpdateCoffeDto) {
+    const existingCoffee = await this.coffeeModel
+      .findOneAndUpdate(
+        { _id: id }, // find query
+        { $set: updateCoffeeDto }, // mongoose update query object
+        { new: true }, // return the update coffee data, by default its false and returns the old
+      )
+      .exec();
+
+    if (!existingCoffee) {
+      throw new NotFoundException();
+    }
+
+    return existingCoffee;
+  }
+
+  async remove(id: string) {
+    const coffee = await this.findOne(id);
+    await coffee.deleteOne();
+  }
 }
